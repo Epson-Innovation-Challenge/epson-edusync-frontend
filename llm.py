ba717d014eae8611b65151c518cc7446ac886a26
whybe-choi <choi980912@khu.ac.kr>
2024-06-22 19:17:19 +0900

    feat: Change Cohere to OpenAI

--- llm.py
+++ llm.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-from langchain_cohere import ChatCohere
+from langchain_openai import ChatOpenAI
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 from prompts import load_generation_prompt
@@ -12,7 +12,7 @@ def generate_question(question):
     Question generation chain.
     """
     prompt = load_generation_prompt()
-    llm = ChatCohere(temperature=0.1, max_tokens=2048)
+    llm = ChatOpenAI(model="gpt-4-turbo-preview", temperature=0.1, max_tokens=2048)
     chain = prompt | llm | StrOutputParser()
     response = chain.invoke({"question": question})
 
